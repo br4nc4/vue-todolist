1c559b001ddec6c6d51313afcc182b3f15e32524
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-13 15:08:07 +0200

    start vue todolist

---
+++ script.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:'#app',
+    data:{
+        toDoList:[
+        {
+            text:"fare la spesa",
+            done: false,
+        },
+        {
+            text:"andare in palestra",
+            done: false,
+        },
+        {
+            text:"pagare bollette",
+            done: false,
+        },
+    ],
+    
+    methods:{
+        
+    }
+    }
+});
